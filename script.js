2367b61f84c12822546fdc38c342a718e134902f
yasin2486 <yasin.ansari@apsissolutions.com>
2025-03-20 23:58:22 +0530

    added color buttons and image dynamically using array of colors

--- script.js
+++ script.js
@@ -1,85 +1,88 @@
-const uploadButton = document.getElementById("upload-button");
-const umbrellaImage = document.getElementById("umbrella-image");
-const removeFileButton = document.getElementById("remove-file");
-const loaderImage = document.getElementById("loader-image");
-const uploadIcon = document.getElementById("upload-icon");
-
-function changeColorAndImage(color, backgroundColor, imageName, buttonId) {
-  const container = document.getElementById("container");
-  const allColorButtons = document.getElementsByClassName("color-button");
-  const selectedButton = document.getElementById(buttonId);
-  const uploadedLogo = document.getElementById("uploaded-logo");
-
-  uploadButton.style.backgroundColor = color; // Change the background color of the upload button
-
-  // Remove the active class from all buttons
-  for (let button of allColorButtons) {
-    button.classList.remove("active");
-  }
-  // add the active class to the selected button
-  selectedButton.classList.add("active");
-
-  // change background color and umbrella image
-  container.style.backgroundColor = backgroundColor;
-
-  umbrellaImage.style.display = "none"; // Hide the umbrella image
-  if (uploadedLogo) {
-    uploadedLogo.style.display = "none"; // Hide uploaded logo
-  }
-  loaderImage.style.display = "block"; // Show the loader image
-
-  setTimeout(function () {
-    loaderImage.style.display = "none"; // Hide the loader image
-    umbrellaImage.style.display = "block"; // Show the umbrella image
-    if (uploadedLogo) {
-      uploadedLogo.style.display = "block"; // Show uploaded logo
-    }
-    umbrellaImage.setAttribute("src", `./assets/${imageName}`);
-  }, 2000);
-}
-
-// Function to handle click on the upload button
-function handleUploadButtonClick() {
+document.addEventListener("DOMContentLoaded", function () {
+  const uploadButton = document.getElementById("upload-button");
   const uploadInput = document.getElementById("file-input");
-  uploadInput.click(); // Simulate a click on the hidden file input element
-}
+  const umbrellaImage = document.getElementById("umbrella-image");
+  const removeFileButton = document.getElementById("remove-file");
+  const loaderImage = document.getElementById("loader-image");
+  const uploadIcon = document.getElementById("upload-icon");
+  const colorButtonsContainer = document.getElementById("color-button-container");
+  const mainContainer = document.getElementById("container");
 
-function handleUploadImage(event) {
-  const file = event.target.files[0];
-  if (file && (file.type === "image/jpeg" || file.type === "image/png") && file.size <= 5242880) {
-    const reader = new FileReader();
+  const colors = [
+    { name: "pink", hex: "#f10fe6", bgColor: "#f5e2f7", image: "./assets/PinkUmbrella.png" },
+    { name: "blue", hex: "#0f8bf1", bgColor: "#c7e3eb", image: "./assets/BlueUmbrella.png" },
+    { name: "yellow", hex: "#f1c80f", bgColor: "#fcfae7", image: "./assets/YellowUmbrella.png" },
+  ];
 
-    reader.onload = function () {
-      const uploadedLogo = document.createElement("img"); // Create a new img element
-      uploadedLogo.src = reader.result; // Set the source of the uploaded image to the file data
-      uploadedLogo.id = "uploaded-logo"; // Add id to the uploaded logo
+  // Dynamically create color buttons
+  colors.forEach((color) => {
+    const button = document.createElement("button");
+    button.style.backgroundColor = color.hex;
+    button.classList.add("color-button");
+    button.addEventListener("click", () => changeUmbrellaImage(color.hex, color.bgColor, color.image));
+    colorButtonsContainer.appendChild(button);
+  });
 
-      umbrellaImage.style.display = "none"; // Hide the umbrella image
-      loaderImage.style.display = "block"; // Show the loader image
-      setTimeout(function () {
-        document.getElementById("umbrella-container").appendChild(uploadedLogo); // Append the uploaded image to the image container
-        loaderImage.style.display = "none"; // Show the loader image
-        umbrellaImage.style.display = "block"; // Hide the umbrella image
-        uploadButton.textContent = file.name; // Change the text of the upload button to the name of the uploaded file
-        uploadButton.appendChild(removeFileButton);
-        removeFileButton.style.display = "block"; // Show the remove file button
-      }, 2000);
-    };
-    reader.readAsDataURL(file); // Read the file as a data URL
+  function changeUmbrellaImage(color, bgColor, imageSrc) {
+    const uploadedLogo = document.getElementById("uploaded-logo");
+
+    umbrellaImage.style.display = "none"; // Hide the umbrella image
+    if (uploadedLogo) {
+      uploadedLogo.style.display = "none"; // Hide uploaded logo
+    }
+    loaderImage.style.display = "block"; // Show the loader image
+    mainContainer.style.backgroundColor = bgColor;
+    uploadButton.style.backgroundColor = color;
+
+    setTimeout(function () {
+      umbrellaImage.src = imageSrc;
+
+      loaderImage.style.display = "none"; // Hide the loader image
+      umbrellaImage.style.display = "block"; // Show the umbrella image
+      if (uploadedLogo) {
+        uploadedLogo.style.display = "block"; // Show uploaded logo
+      }
+    }, 2000);
   }
-}
 
-const uploadInput = document.getElementById("file-input");
-uploadInput.addEventListener("change", handleUploadImage);
+  function handleUploadImage(event) {
+    const file = event.target.files[0];
+    if (file && (file.type === "image/jpeg" || file.type === "image/png") && file.size <= 5242880) {
+      const reader = new FileReader();
 
-removeFileButton.addEventListener("click", (e) => {
-  e.stopPropagation();
+      reader.onload = function () {
+        const uploadedLogo = document.createElement("img"); // Create a new img element
+        uploadedLogo.src = reader.result; // Set the source of the uploaded image to the file data
+        uploadedLogo.id = "uploaded-logo"; // Add id to the uploaded logo
 
-  uploadInput.value = ""; // Clear the file input
-  uploadButton.textContent = "Upload Image";
-  uploadButton.appendChild(uploadIcon);
-  uploadButton.appendChild(removeFileButton);
-  const uploadedLogo = document.getElementById("uploaded-logo");
-  uploadedLogo.remove();
-  removeFileButton.style.display = "none";
+        umbrellaImage.style.display = "none"; // Hide the umbrella image
+        loaderImage.style.display = "block"; // Show the loader image
+        setTimeout(function () {
+          document.getElementById("umbrella-container").appendChild(uploadedLogo); // Append the uploaded image to the image container
+          loaderImage.style.display = "none"; // Show the loader image
+          umbrellaImage.style.display = "block"; // Hide the umbrella image
+          uploadButton.textContent = file.name; // Change the text of the upload button to the name of the uploaded file
+          uploadButton.appendChild(removeFileButton);
+          removeFileButton.style.display = "block"; // Show the remove file button
+        }, 2000);
+      };
+      reader.readAsDataURL(file); // Read the file as a data URL
+    }
+  }
+
+  // File Upload Handling
+  uploadButton.addEventListener("click", () => uploadInput.click());
+  uploadInput.addEventListener("change", handleUploadImage);
+
+  removeFileButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+
+    uploadInput.value = ""; // Clear the file input
+    uploadButton.textContent = "Upload Image";
+    uploadButton.appendChild(uploadIcon);
+    uploadButton.appendChild(removeFileButton);
+    const uploadedLogo = document.getElementById("uploaded-logo");
+    uploadedLogo.remove();
+    removeFileButton.style.display = "none";
+  });
 });
